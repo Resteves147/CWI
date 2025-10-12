882d72c673cc9a1435a62089557718f69ef8a7db
Resteves147 <resteves147@gmail.com>
2025-10-11 22:42:03 -0700

    added images

---
+++ src/pages/homepage.jsx
@@ -0,0 +1,15 @@
+const Homepage = () => {
+    return (
+        <section className ="Body">
+            <div className ="Image">
+
+            </div>
+
+            <div className ="About">
+                
+            </div>
+        </section>
+    );
+}
+
+export default Homepage;
